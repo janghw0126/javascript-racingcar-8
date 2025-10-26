0ccb56d8680195f92ad831ac76c84938f97dd0f5
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-10-26 14:49:02 +0900

    feat(App): validateCarName 함수 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ class App {
   async run() {
     const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
   }
+
+  // 자동차 이름 검증 함수
+  validateCarName(input){
+     // 자동차 이름 분리
+    let carNames = input.split(",");
+
+    return carNames;
+
+  };
 }
 
 export default App;
