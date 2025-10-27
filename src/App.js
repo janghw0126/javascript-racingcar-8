171098698d3d6db0f54df212e96774fefd54026d
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-10-27 19:48:00 +0900

    fix(App): 불필요한 주석 제거 및 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   async run() {
     const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
 
-    // 자동차 이름 검증 함수 실행 및 이름 추출
+    // 자동차 이름 검증 및 추출
     const carNames = this.validateCarName(input);
 
     // 자동차 이동 상태 객체 선언
@@ -13,14 +13,14 @@ class App {
       carStates[name] = 0;
     });
 
-    // 시도 횟수 입력
+    // 시도 횟수 입력 및 검증
     const raceCount = Number(await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n"));
     this.validateRaceCount(raceCount);
 
     Console.print("");
     Console.print("실행 결과");
 
-    // 시도할 횟수만큼 반복하면서 경주 게임 실행
+    // 시도 횟수만큼 반복하면서 경주 게임 실행
     for (let i = 0; i < raceCount; i++) {
       this.checkCarMovement(carStates);
       this.printCarStates(carStates);
@@ -30,7 +30,7 @@ class App {
     Console.print(`최종 우승자 : ${this.determineWinner(carStates).join(", ")}`);
   }
 
-  // 자동차 이름 검증 함수
+  // 자동차 이름 검증
   validateCarName(input){
     if(/\s/.test(input)){
       throw new Error("[ERROR] 입력값 사이에 공백이 있습니다.");
@@ -60,7 +60,7 @@ class App {
     return carNames;
   };
 
-  // 시도 횟수 검증 함수
+  // 시도 횟수 검증
   validateRaceCount(raceCount){
     if(raceCount===""){
       throw new Error("[ERROR] 시도 횟수가 비어있습니다.");
@@ -72,9 +72,8 @@ class App {
       throw new Error("[ERROR] 0 이하의 숫자를 입력하였습니다.");
     }
   }
-  
 
-  // 자동차 전진 여부 판단 함수
+  // 자동차 전진 여부 판단
   checkCarMovement(carStates){
     for (const name in carStates){
       let randomNumber = Random.pickNumberInRange(0, 9);
@@ -84,19 +83,19 @@ class App {
     }
   }
 
-  // 전진 함수 추가 함수
+  // 자동차 한 칸 전진
   moveCar(carStates,name){
     carStates[name] += 1;
   }
 
-  // 차 진행상황 출력 함수 추가 함수
+  // 현재 라운드별 자동차 이동상태 출력
   printCarStates(carStates) {
     Object.entries(carStates).forEach(([name,position]) => {
       Console.print(`${name} : ${"-".repeat(position)}`);
     });
   }
 
-  // 우승자 판단 함수 추가
+  // 우승자 판단
   determineWinner(carStates){
     const maxPosition = Math.max(...Object.values(carStates));
     const winner = Object.keys(carStates).filter(
